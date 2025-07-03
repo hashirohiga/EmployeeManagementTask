570871b92fd13a153450ca85b9620006c57b3950
DmitrySerovd <d.serov@sh.dunice.net>
2025-07-04 02:19:02 +0300

    employees-endpoints: refact

--- src/EmployeeManagementTask.Application/Handlers/Queries/GetEmployees/GetEmployeesQuery.cs
+++ src/EmployeeManagementTask.Application/Handlers/Queries/GetEmployees/GetEmployeesQuery.cs
@@ -5,7 +5,10 @@ namespace EmployeeManagementTask.Application.Handlers.Queries.GetEmployees;
 public class GetEmployeesQuery : IRequest<GetEmployeesQueryResult>
 {
     public string? Department { get; set; }
+
     public string? FullName { get; set; }
+
     public DateTime? DateOfBirth { get; set; }
+
     public DateTime? HireDate { get; set; }
 }
--- src/EmployeeManagementTask.WebApi/Requests/GetEmployeesRequest.cs
+++ src/EmployeeManagementTask.WebApi/Requests/GetEmployeesRequest.cs
@@ -3,8 +3,11 @@
     public class GetEmployeesRequest
     {
         public string? Department { get; set; }
+
         public string? FullName { get; set; }
-        public DateTime? BirthDate { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
         public DateTime? HireDate { get; set; }
     }
 }
